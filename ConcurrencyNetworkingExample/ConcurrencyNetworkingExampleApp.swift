6d0bf56e9870f05100e87e7fb2951e4f99257bf3
Scott Gardner <scott.gardner@mac.com>
2024-07-02 13:15:11 -0500

    Initial Commit

---
+++ ConcurrencyNetworkingExample/ConcurrencyNetworkingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyNetworkingExampleApp.swift
+//  ConcurrencyNetworkingExample
+//
+//  Created by Scott Gardner on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyNetworkingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
